60bf4eb08bed15e6d5d5d22da2efc89bc6dc3827
dhbaek2004 <dhbaek2004@gmail.com>
2021-09-10 22:40:45 +0900

    Initial commit

---
+++ src/main/java/BaekInternational/ManhattanLittleBoy/ManhattanLittleBoyApplication.java
@@ -0,0 +1,13 @@
+package BaekInternational.ManhattanLittleBoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManhattanLittleBoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManhattanLittleBoyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BaekInternational/ManhattanLittleBoy/ManhattanLittleBoyApplicationTests.java
@@ -0,0 +1,13 @@
+package BaekInternational.ManhattanLittleBoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManhattanLittleBoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
