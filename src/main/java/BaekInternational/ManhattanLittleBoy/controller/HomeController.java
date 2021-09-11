afa7720ab45bcfa982af7c5897a7477259322cfd
chan <lec0428@naver.com>
2021-09-11 22:57:51 +0900

    test

--- src/main/java/BaekInternational/ManhattanLittleBoy/controller/HomeController.java
+++ src/main/java/BaekInternational/ManhattanLittleBoy/controller/HomeController.java
@@ -3,6 +3,7 @@ package BaekInternational.ManhattanLittleBoy.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,7 +21,4 @@ public class HomeController {
 
         return "hello";
     }
-
-
-
 }
