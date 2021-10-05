b54fb70bc70fb1903cabd44a1f0ef45482dcf94a
Euichan <lec0428@naver.com>
2021-10-05 21:49:14 +0900

    QuestionController Test

--- src/main/java/BaekInternational/ManhattanLittleBoy/controller/QuestionController.java
+++ src/main/java/BaekInternational/ManhattanLittleBoy/controller/QuestionController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +28,7 @@ public class QuestionController {
 
     // 질문 등록하기
     @PostMapping("/post")
-    public String postQuetions(QuestionForm form) {
+    public String postQuetions(QuestionForm form, Model model) {
         Question question = new Question();
         question.setQuestionTitle(form.getTitle());
         // TODO 세션, 글내용 가져오기
