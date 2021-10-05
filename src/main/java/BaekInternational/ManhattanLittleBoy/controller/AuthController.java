8d7aac0cbd369871ecbaffe2fc4b77e34ae69779
dhbaek2004 <dhbaek2004@naver.com>
2021-10-05 23:51:43 +0900

    edit Auth Controller

--- src/main/java/BaekInternational/ManhattanLittleBoy/controller/AuthController.java
+++ src/main/java/BaekInternational/ManhattanLittleBoy/controller/AuthController.java
@@ -1,4 +1,27 @@
 package BaekInternational.ManhattanLittleBoy.controller;
 
+import BaekInternational.ManhattanLittleBoy.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/auth")
 public class AuthController {
+
+    private MemberRepository memberRepository;
+
+    public void MemberController(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login/login";
+    }
+
 }
