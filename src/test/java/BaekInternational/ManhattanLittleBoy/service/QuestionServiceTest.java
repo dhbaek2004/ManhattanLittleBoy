e5542d228b116039f2b5f1441f5f5ebd13b64b3f
Euichan Lee <euichan.lee@tysystems.com>
2021-10-14 13:58:02 +0900

    통합 테스트 코드 -> 단위 테스트 코드 변경 작업중

--- src/test/java/BaekInternational/ManhattanLittleBoy/service/QuestionServiceTest.java
+++ src/test/java/BaekInternational/ManhattanLittleBoy/service/QuestionServiceTest.java
@@ -1,26 +1,32 @@
 package BaekInternational.ManhattanLittleBoy.service;
 
 import BaekInternational.ManhattanLittleBoy.domain.Question;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import BaekInternational.ManhattanLittleBoy.repository.QuestionRepository;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
-@Transactional
-public class QuestionServiceTest {
+// @SpringBootTest // 통합테스트임 , 통합테스트보단 단위테스트가 좋다 -김0한
+// @Transactional
+class QuestionServiceTest {
     LocalDateTime currentDateTime = LocalDateTime.now();
-    @Autowired QuestionService questionService;
+
+
+    @BeforeEach
+    void beforeEach() {
+        QuestionService questionService;
+        QuestionRepository questionRepository;
+
+    }
+
+    @AfterEach
+    void afterEach() {
+
+    }
 
     @Test
     void 질문등록() {
@@ -29,18 +35,20 @@ public class QuestionServiceTest {
         question.setQuestionTitle("질문 제목");
         question.setQuestionContent("질문 내용");
         question.setQuestionRegisterTime(currentDateTime);
+        // questionService.createQuestion(question);
 
         //when
-        questionService.createQuestion(question);
 
         //then
-        List<Question> qaa = questionService.findAllQuestions();
-        System.out.println("아놔 이 쉬바" + qaa);
 
     }
 
     @Test
     void 모든질문출력() {
+//        List<Question> questionList = questionService.findAllQuestions();
+//        for(int i=0; i<questionList.size(); i++) {
+//            System.out.println(questionList.get(i));
+//        }
     }
 
     @Test
